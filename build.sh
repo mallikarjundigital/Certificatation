07de598ef306c4b66d489830c8d4a63ce21484fa
mallikarjun.digital21 <mallikarjun.digital21@gmail.com>
2021-07-01 20:13:47 +0000

    buid.sh changes

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
+sudo docker rm -f $(docker ps -a -q)
 sudo docker build -t devopsedu/webapp-php:0.0 .
-sudo docker run -it -d -p 8001:80 devopsedu/webapp:0.0
+sudo docker run -it -d -p 8001:80 devopsedu/webapp-php:0.0
